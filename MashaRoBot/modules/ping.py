ff1f1111689d085cf86210d6fd2fbc87c4c2a7cc
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-03 11:27:57 +0700

    Update ping.py

--- MashaRoBot/modules/ping.py
+++ MashaRoBot/modules/ping.py
@@ -81,8 +81,8 @@ def ping(update: Update, context: CallbackContext):
 
     message.edit_text(
         "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>⏱Time Taken:</b> <code>{}</code>\n"
+        "<b>🧑‍🔧Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 

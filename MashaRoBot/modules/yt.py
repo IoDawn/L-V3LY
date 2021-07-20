faa6a0380fcd8c474113b90f336aca10f610867e
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-20 21:53:31 +0700

    Update yt.py

--- MashaRoBot/modules/yt.py
+++ MashaRoBot/modules/yt.py
@@ -1,10 +1,6 @@
 __help__ = """
 Here is help for downloading audio|video from YouTube
 - /ytaudio <link> 
-Example - "/ytaudio https://youtu.be/i0_m90T04uw"
-Aᴜᴅɪᴏ ɪsɴ'ᴛ sᴜᴘᴘᴏʀᴛɪɴɢ , ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴠɪᴅᴇᴏ
-
 - /ytvideo <link> 
-Example - "/ytvideo https://youtu.be/i0_m90T04uw"
 """
 __mod_name__ = "YouTube"
